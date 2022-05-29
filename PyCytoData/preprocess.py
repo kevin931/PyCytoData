62b9d6af27b49f61a2ae32afb6ee55ffc4727221
Kevin Wang <kevinwang@smu.edu>
2022-05-29 17:01:34 -0500

    Add indices as return type

--- PyCytoData/preprocess.py
+++ PyCytoData/preprocess.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from numpy.typing import ArrayLike
-from typing import Optional, List
+from typing import Optional, List, Tuple
 
 
 def arcsinh(data: ArrayLike, channels: Optional[ArrayLike]=None, transform_channels: Optional[ArrayLike]=None, cofactor: int=5) -> np.ndarray:
@@ -35,7 +35,7 @@ def arcsinh(data: ArrayLike, channels: Optional[ArrayLike]=None, transform_chann
     return data
 
 
-def gate_debris_removal(data: ArrayLike, channels: ArrayLike, bead_channels: ArrayLike) -> np.ndarray:
+def gate_debris_removal(data: ArrayLike, channels: ArrayLike, bead_channels: ArrayLike) -> Tuple[np.ndarray, np.ndarray]:
     """Pre-gating to remove debris.
 
     This is a first step of the gating procedures to remove debris. Channels names and bead channel names are needed.
@@ -44,7 +44,7 @@ def gate_debris_removal(data: ArrayLike, channels: ArrayLike, bead_channels: Arr
     :param channels: The channel names of the expression matrix in the order of the columns, defaults to None
     :param bead_channels: The bead channels as specify by name, defaults to None
     
-    :return: The gated expression matrix.
+    :return: The gated expression matrix or an array of indices. 
     """
     if not isinstance(data, np.ndarray):
         data = np.array(data)
@@ -59,10 +59,15 @@ def gate_debris_removal(data: ArrayLike, channels: ArrayLike, bead_channels: Arr
     bead_exp: np.ndarray = np.mean(data[:, np.isin(channels, bead_channels)], axis=1)
     cutoff_bead: float = np.mean(bead_exp) + np.std(bead_exp)*3
     
-    return data[bead_exp < cutoff_bead,:]
+    indices: np.ndarray = np.arange(0, data.shape[0])
+    return_tup: List = list()
+    return_tup.append(data[bead_exp < cutoff_bead,:])
+    return_tup.append(indices[bead_exp < cutoff_bead])
+
+    return tuple(return_tup)
     
     
-def gate_intact_cells(data: ArrayLike, channels: ArrayLike, DNA_channels: ArrayLike, cutoff_DNA_sd: float=2) -> np.ndarray:
+def gate_intact_cells(data: ArrayLike, channels: ArrayLike, DNA_channels: ArrayLike, cutoff_DNA_sd: float=2) -> Tuple[np.ndarray, np.ndarray]:
     """Gating for intact cells.
 
     This gating procedure gates for intact cells following the debris removal procedure.
@@ -71,9 +76,9 @@ def gate_intact_cells(data: ArrayLike, channels: ArrayLike, DNA_channels: ArrayL
     :param data: The expression matrix array of two dimensions.
     :param channels: The channel names of the expression matrix in the order of the columns, defaults to None
     :param DNA_channels: The DNA channels as specify by name, defaults to None
-    :param cutoff_DNA_sd: The number of standard deviations away from the mean to use as a cutoff for DNA channels.
+    :param cutoff_DNA_sd: The number of standard deviations away from the mean to use as a cutoff for DNA channels, defaults to 2.
     
-    :return: The gated expression matrix.
+    :return: A tuple of the gated expression matrix and indices based on original data.
     """
     
     if not isinstance(data, np.ndarray):
@@ -94,13 +99,22 @@ def gate_intact_cells(data: ArrayLike, channels: ArrayLike, DNA_channels: ArrayL
     cutoff_DNA2_upper: float = np.mean(data[:,np.isin(channels, DNA_channels[1])]) + cutoff_DNA2_se
     cutoff_DNA2_lower: float = np.mean(data[:,np.isin(channels, DNA_channels[1])]) - cutoff_DNA2_se
     
-    return data[np.where((data[:,np.isin(channels, DNA_channels[0])] > cutoff_DNA1_lower).flatten() & 
-                         (data[:,np.isin(channels, DNA_channels[0])] < cutoff_DNA1_upper).flatten() & 
-                         (data[:,np.isin(channels, DNA_channels[1])] > cutoff_DNA2_lower).flatten() &
-                         (data[:,np.isin(channels, DNA_channels[1])] < cutoff_DNA2_upper).flatten())[0],:]
+    indices: np.ndarray = np.arange(0, data.shape[0])
+    return_tup: List = list()
+    return_tup.append(data[np.where((data[:,np.isin(channels, DNA_channels[0])] > cutoff_DNA1_lower).flatten() & 
+                                    (data[:,np.isin(channels, DNA_channels[0])] < cutoff_DNA1_upper).flatten() & 
+                                    (data[:,np.isin(channels, DNA_channels[1])] > cutoff_DNA2_lower).flatten() &
+                                    (data[:,np.isin(channels, DNA_channels[1])] < cutoff_DNA2_upper).flatten())[0],:])
+    return_tup.append(indices[np.where((data[:,np.isin(channels, DNA_channels[0])] > cutoff_DNA1_lower).flatten() & 
+                                       (data[:,np.isin(channels, DNA_channels[0])] < cutoff_DNA1_upper).flatten() & 
+                                       (data[:,np.isin(channels, DNA_channels[1])] > cutoff_DNA2_lower).flatten() &
+                                       (data[:,np.isin(channels, DNA_channels[1])] < cutoff_DNA2_upper).flatten())[0],:])
+    
+    return tuple(return_tup)
+
     
     
-def gate_live_cells(data: ArrayLike, channels: ArrayLike, dead_channel: ArrayLike, cutoff_quantile: float=0.03) -> np.ndarray:
+def gate_live_cells(data: ArrayLike, channels: ArrayLike, dead_channel: ArrayLike, cutoff_quantile: float=0.03) -> Tuple[np.ndarray, np.ndarray]:
     """Gating for live cells.
 
     This gating procedure gates for living cells following the gating procedure for intact cells.
@@ -109,9 +123,9 @@ def gate_live_cells(data: ArrayLike, channels: ArrayLike, dead_channel: ArrayLik
     :param data: The expression matrix array of two dimensions.
     :param channels: The channel names of the expression matrix in the order of the columns, defaults to None
     :param dead_channel: The dead channels as specify by name, defaults to None
-    :param cutoff_quantile: The top quantile to be excluded.
+    :param cutoff_quantile: The top quantile to be excluded, defaults to 0.03.
     
-    :return: The gated expression matrix.
+    :return: A tuple of the gated expression matrix and indices based on original data.
     
     :raises ValueError: More than 1 "Dead" channel provided.
     """
@@ -130,21 +144,28 @@ def gate_live_cells(data: ArrayLike, channels: ArrayLike, dead_channel: ArrayLik
         raise ValueError("Only 1 'Dead' channel allowed.")
     
     cutoff_dead: float = np.quantile(data[:,np.isin(channels, dead_channel)], 1-cutoff_quantile)
-    return data[(data[:, np.isin(channels, dead_channel)] < cutoff_dead).flatten(),:]
+    
+    indices: np.ndarray = np.arange(0, data.shape[0])
+    return_tup: List = list()
+    return_tup.append(data[(data[:, np.isin(channels, dead_channel)] < cutoff_dead).flatten(),:])
+    return_tup.append(indices[(data[:, np.isin(channels, dead_channel)] < cutoff_dead).flatten(),:])
+    
+    return tuple(return_tup)
     
     
-def gate_center_offset_residual(data: ArrayLike, channels: ArrayLike, cor_channels: ArrayLike, cutoff_quantile: float=0.03):
-    """Gating for live cells.
+    
+def gate_center_offset_residual(data: ArrayLike, channels: ArrayLike, cor_channels: ArrayLike, cutoff_quantile: float=0.03) -> Tuple[np.ndarray, np.ndarray]:
+    """Gating for center, offser, and residual cells.
 
-    This gating procedure gates for living cells following the gating procedure for intact cells.
-    All channel names and 'Dead' channel names needed.
+    This gating procedure gates for cells using the center, offset, and residual channels.
+    All channel names and the three channels are needed.
 
     :param data: The expression matrix array of two dimensions.
     :param channels: The channel names of the expression matrix in the order of the columns, defaults to None
     :param cor_channels: The center, offset, and residual channels as specify by name, defaults to None
-    :param cutoff_quantile: The top and bottom quantile to be excluded.
+    :param cutoff_quantile: The top and bottom quantile to be excluded, defaults to 0.03.
     
-    :return: The gated expression matrix.
+    :return: A tuple of the gated expression matrix and indices based on original data.
     """
     if not isinstance(data, np.ndarray):
         data = np.array(data)
@@ -162,15 +183,27 @@ def gate_center_offset_residual(data: ArrayLike, channels: ArrayLike, cor_channe
     cutoff1_lower: float = np.quantile(data[:,np.isin(channels, cor_channels[1])], cutoff_quantile)
     cutoff2_upper: float = np.quantile(data[:,np.isin(channels, cor_channels[2])], 1-cutoff_quantile)
     cutoff2_lower: float = np.quantile(data[:,np.isin(channels, cor_channels[2])], cutoff_quantile)
-    return data[np.where((data[:,np.isin(channels, cor_channels[0])] > cutoff0_lower).flatten() & 
-                         (data[:,np.isin(channels, cor_channels[0])] < cutoff0_upper).flatten() &
-                         (data[:,np.isin(channels, cor_channels[1])] > cutoff1_lower).flatten() & 
-                         (data[:,np.isin(channels, cor_channels[1])] < cutoff1_upper).flatten() &
-                         (data[:,np.isin(channels, cor_channels[2])] > cutoff2_lower).flatten() & 
-                         (data[:,np.isin(channels, cor_channels[2])] < cutoff2_upper).flatten())[0],:]
     
+    indices: np.ndarray = np.arange(0, data.shape[0])
+    return_tup: List = list()
+    return_tup.append(data[np.where((data[:,np.isin(channels, cor_channels[0])] > cutoff0_lower).flatten() & 
+                                    (data[:,np.isin(channels, cor_channels[0])] < cutoff0_upper).flatten() &
+                                    (data[:,np.isin(channels, cor_channels[1])] > cutoff1_lower).flatten() & 
+                                    (data[:,np.isin(channels, cor_channels[1])] < cutoff1_upper).flatten() &
+                                    (data[:,np.isin(channels, cor_channels[2])] > cutoff2_lower).flatten() & 
+                                    (data[:,np.isin(channels, cor_channels[2])] < cutoff2_upper).flatten())[0],:])
     
-def bead_normalization(data: ArrayLike, channels: ArrayLike, bead_channels: ArrayLike, time_channel: ArrayLike, transform_channels: ArrayLike):
+    return_tup.append(indices[np.where((data[:,np.isin(channels, cor_channels[0])] > cutoff0_lower).flatten() & 
+                                       (data[:,np.isin(channels, cor_channels[0])] < cutoff0_upper).flatten() &
+                                       (data[:,np.isin(channels, cor_channels[1])] > cutoff1_lower).flatten() & 
+                                       (data[:,np.isin(channels, cor_channels[1])] < cutoff1_upper).flatten() &
+                                       (data[:,np.isin(channels, cor_channels[2])] > cutoff2_lower).flatten() & 
+                                       (data[:,np.isin(channels, cor_channels[2])] < cutoff2_upper).flatten())[0],:])
+    
+    return tuple(return_tup)
+    
+
+def bead_normalization(data: ArrayLike, channels: ArrayLike, bead_channels: ArrayLike, time_channel: ArrayLike, transform_channels: ArrayLike) -> Tuple[np.ndarray, np.ndarray]:
     """Bead normalization to correct time-shift throughout an experiment.
 
     Sometimes, CyTOF has a phenomenon known as time-dependent shift, meaning that the over expression
@@ -182,7 +215,7 @@ def bead_normalization(data: ArrayLike, channels: ArrayLike, bead_channels: Arra
     :param time_channel: The time channels as specify by name
     :param transform_channels: The transform channels to apply the normalization as specify by name
     
-    :return: The normalized expression matrix.
+    :return: A tuple of the normalized expression matrix and indices based on original data.
     
     :raises ValueError: More than 1 "Time" channel provided.
     """
@@ -205,6 +238,7 @@ def bead_normalization(data: ArrayLike, channels: ArrayLike, bead_channels: Arra
     if time_channel.shape[0] > 1:
         raise ValueError("Only 1 'Time' channel allowed.")
     
+    indices: np.ndarray = np.arange(0, data.shape[0])
     intervals: np.ndarray = np.round(data[:,np.isin(channels, time_channel)]/np.max(data[:,np.isin(channels, time_channel)])*200).flatten()
     unique_intervals: np.ndarray = np.unique(intervals)
     
@@ -220,6 +254,7 @@ def bead_normalization(data: ArrayLike, channels: ArrayLike, bead_channels: Arra
     keep = keep > (bead_channels.shape[0]/2)
     unique_intervals = unique_intervals[keep]
     data = data[np.isin(intervals, unique_intervals),:]
+    indices = indices[np.isin(intervals, unique_intervals)]
     
     # Normalization
     intervals: np.ndarray = np.round(data[:,np.isin(channels, time_channel)]/np.max(data[:,np.isin(channels, time_channel)])*200).flatten() # type: ignore
@@ -234,9 +269,8 @@ def bead_normalization(data: ArrayLike, channels: ArrayLike, bead_channels: Arra
         np.fill_diagonal(cormat, -1)
         cormat[np.tril_indices_from(cormat)] = -1
         max_cor: int = np.argmax(cormat) #type: ignore
-        indices: List[int] = list(np.unravel_index(max_cor, [cormat.shape[0], cormat.shape[1]])) #type: ignore
-        bead_channels = bead_channels[indices]
-        
+        cor_indices: List[int] = list(np.unravel_index(max_cor, [cormat.shape[0], cormat.shape[1]])) #type: ignore
+        bead_channels = bead_channels[cor_indices]
         
     correct_f: np.ndarray = np.mean(interval_mean[:, np.isin(channels, bead_channels)], axis=1)
     correct_f_full: np.ndarray = np.empty(intervals.shape[0])
@@ -247,4 +281,5 @@ def bead_normalization(data: ArrayLike, channels: ArrayLike, bead_channels: Arra
     
     for c in transform_channels:
         data[:, np.isin(channels, c)] = data[:, np.isin(channels, c)]/(correct_f_full.reshape(data.shape[0], 1)) #type: ignore
-    return data[keep,:] #type: ignore
+        
+    return tuple(data[keep,:], indices[keep]) #type: ignore
